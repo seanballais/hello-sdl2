89435dc4eee1c9a83ef4bc7daaca43ec925c3698
Sean Francis N. Ballais <sfballais123@gmail.com>
2020-04-11 18:47:57 +0800

    :sparkles: Add code for the second lesson of the Lazy Foo SDL 2 tutorial.

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 .DEFAULT_GOAL := all
 .PHONY: all setup_data clean
 
-all: main
+all: app
 
-main: setup_data hello-sdl2/main.cpp
+app: setup_data hello-sdl2/main.cpp
 	g++ hello-sdl2/main.cpp -o bin/hello-sdl2 -lSDL2
 
 setup_data:
--- hello-sdl2/main.cpp
+++ hello-sdl2/main.cpp
@@ -5,35 +5,82 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+SDL_Window* gWindow = nullptr;
+SDL_Surface* gScreenSurface = nullptr;
+SDL_Surface* gHelloSDL2Img = nullptr;
+
+bool initApp();
+bool loadMedia();
+void closeApp();
+
 int main(int argc, char* args[])
 {
-  SDL_Window* window = nullptr;
-  SDL_Surface* screenSurface = nullptr;
+  if (!initApp()) {
+    std::cout << "Failed to initialize." << std::endl;
+  } else {
+    if (!loadMedia()) {
+      std::cout << "Failed to load media." << std::endl;
+    } else {
+      SDL_BlitSurface(gHelloSDL2Img, NULL, gScreenSurface, NULL);
+      SDL_UpdateWindowSurface(gWindow);
+      SDL_Delay(2000);
+    }
+  }
+
+  closeApp();
+
+  return 0;
+}
+
+bool initApp()
+{
+  bool initSuccessState = true;
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     std::cout << "SDL could not be initialized. SDL Error: " << SDL_GetError()
               << std::endl;
+    initSuccessState = false;
   } else {
-    window = SDL_CreateWindow("Hello SDL 2",
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SCREEN_WIDTH,
-                              SCREEN_HEIGHT,
-                              SDL_WINDOW_SHOWN);
-    if (window == nullptr) {
+    gWindow = SDL_CreateWindow("Hello SDL 2",
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SCREEN_WIDTH,
+                               SCREEN_HEIGHT,
+                               SDL_WINDOW_SHOWN);
+    if (gWindow == nullptr) {
       std::cout << "Window could not be created. SDL Error: " << SDL_GetError()
                 << std::endl;
+      initSuccessState = false;
     } else {
-      screenSurface = SDL_GetWindowSurface(window);
-
-      SDL_FillRect(screenSurface, NULL,
-                   SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-      SDL_UpdateWindowSurface(window);
-      SDL_Delay(2000);
-      SDL_DestroyWindow(window);
-      SDL_Quit();
-
-      return 0;
+      gScreenSurface = SDL_GetWindowSurface(gWindow);
     }
   }
+
+  return initSuccessState;
+}
+
+bool loadMedia()
+{
+  bool loadingSuccessState = true;
+
+  const char* imagePath = "data/hello-sdl2.bmp";
+  gHelloSDL2Img = SDL_LoadBMP(imagePath);
+  if (gHelloSDL2Img == nullptr) {
+    std::cout << "Unable to load image, " << imagePath << ". SDL Error: "
+              << SDL_GetError() << std::endl;
+    loadingSuccessState = false;
+  }
+
+  return loadingSuccessState;
+}
+
+void closeApp()
+{
+  SDL_FreeSurface(gHelloSDL2Img);
+  gHelloSDL2Img = nullptr;
+
+  SDL_DestroyWindow(gWindow);
+  gWindow = nullptr;
+
+  SDL_Quit();
 }
