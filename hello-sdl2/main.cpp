8b13b881e1c77054469c8711d9ad1a2b1a853e04
Sean Francis N. Ballais <sfballais123@gmail.com>
2020-04-11 20:22:59 +0800

    :sparkles: Add code for the fourth lesson of the Lazy Foo SDL 2 tutorial.

--- hello-sdl2/main.cpp
+++ hello-sdl2/main.cpp
@@ -5,13 +5,27 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+enum KeyPressSurfaces
+{
+  KEY_PRESS_SURFACE_DEFAULT,
+  KEY_PRESS_SURFACE_UP,
+  KEY_PRESS_SURFACE_DOWN,
+  KEY_PRESS_SURFACE_LEFT,
+  KEY_PRESS_SURFACE_RIGHT,
+  KEY_PRESS_SURFACE_TOTAL
+};
+
 SDL_Window* gWindow = nullptr;
 SDL_Surface* gScreenSurface = nullptr;
-SDL_Surface* gHelloSDL2Img = nullptr;
+SDL_Surface* gCurrentSurface = nullptr;
+
+SDL_Surface* gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
 
 bool initApp();
 bool loadMedia();
 void closeApp();
+
+SDL_Surface* loadSurface(std::string path);
 
 int main(int argc, char* args[])
 {
@@ -23,18 +37,44 @@ int main(int argc, char* args[])
     } else {
       bool shouldAppQuit = false;
       SDL_Event event;
+      gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
       while (!shouldAppQuit) {
         while (SDL_PollEvent(&event) != 0) {
           switch (event.type) {
             case SDL_QUIT:
               shouldAppQuit = true;
               break;
+
+            case SDL_KEYDOWN:
+              switch (event.key.keysym.sym) {
+                case SDLK_UP:
+                  gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_UP];
+                  break;
+
+                case SDLK_DOWN:
+                  gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN];
+                  break;
+
+                case SDLK_LEFT:
+                  gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT];
+                  break;
+
+                case SDLK_RIGHT:
+                  gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT];
+                  break;
+
+                default:
+                  gCurrentSurface = gKeyPressSurfaces[
+                                      KEY_PRESS_SURFACE_DEFAULT];
+                  break;
+              }
+              break;
             default:
               continue;
           }
         }
 
-        SDL_BlitSurface(gHelloSDL2Img, NULL, gScreenSurface, NULL);
+        SDL_BlitSurface(gCurrentSurface, NULL, gScreenSurface, NULL);
         SDL_UpdateWindowSurface(gWindow);
       }
     }
@@ -76,12 +116,26 @@ bool loadMedia()
 {
   bool loadingSuccessState = true;
 
-  const char* imagePath = "data/hello-sdl2.bmp";
-  gHelloSDL2Img = SDL_LoadBMP(imagePath);
-  if (gHelloSDL2Img == nullptr) {
-    std::cout << "Unable to load image, " << imagePath << ". SDL Error: "
-              << SDL_GetError() << std::endl;
-    loadingSuccessState = false;
+  const char* defaultImagePath = "data/hello-sdl2.bmp";
+  gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface(defaultImagePath);
+
+  const char* upImagePath = "data/key_presses/up.bmp";
+  gKeyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface(upImagePath);
+
+  const char* downImagePath = "data/key_presses/down.bmp";
+  gKeyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface(downImagePath);
+
+  const char* leftImagePath = "data/key_presses/left.bmp";
+  gKeyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface(leftImagePath);
+
+  const char* rightImagePath = "data/key_presses/right.bmp";
+  gKeyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface(rightImagePath);
+
+  for (auto*& surface : gKeyPressSurfaces) {
+    if (surface == nullptr) {
+      loadingSuccessState = false;
+      break;
+    }
   }
 
   return loadingSuccessState;
@@ -89,11 +143,24 @@ bool loadMedia()
 
 void closeApp()
 {
-  SDL_FreeSurface(gHelloSDL2Img);
-  gHelloSDL2Img = nullptr;
+  for (auto*& surface : gKeyPressSurfaces) {
+    SDL_FreeSurface(surface);
+    surface = nullptr;
+  }
 
   SDL_DestroyWindow(gWindow);
   gWindow = nullptr;
 
   SDL_Quit();
+}
+
+SDL_Surface* loadSurface(std::string path)
+{
+  SDL_Surface* surface = SDL_LoadBMP(path.c_str());
+  if (surface == nullptr) {
+    std::cout << "Unable to load BMP image, " << path << ". SDL Error: "
+              << SDL_GetError() << std::endl;
+  }
+
+  return surface;
 }
