9f4304b70a1fc0ac34e31e8cd839d48ee22ec066
Sean Francis N. Ballais <sfballais123@gmail.com>
2020-04-12 18:29:03 +0800

    :sparkles: Add code for the tenth lesson of the Lazy Foo SDL 2 tutorial.

--- hello-sdl2/main.cpp
+++ hello-sdl2/main.cpp
@@ -8,7 +8,87 @@ const int SCREEN_HEIGHT = 480;
 
 SDL_Window* gWindow = nullptr;
 SDL_Renderer* gRenderer = nullptr;
-SDL_Texture* gTexture = nullptr;
+
+class LTexture
+{
+public:
+  LTexture()
+    : texture(nullptr)
+    , width(0)
+    , height(0) {}
+  
+  ~LTexture()
+  {
+    free();
+  }
+
+  bool loadFromFile(std::string path)
+  {
+    free();
+
+    SDL_Texture* texture = nullptr;
+
+    SDL_Surface* surface = IMG_Load(path.c_str());
+    if (surface == nullptr) {
+      std::cout << "Unable to load image, " << path << ". SDL_image Error: "
+                << IMG_GetError() << std::endl;
+    } else {
+      SDL_SetColorKey(surface, SDL_TRUE,
+                      SDL_MapRGB(surface->format, 0, 0xFF, 0xFF));
+
+      texture = SDL_CreateTextureFromSurface(gRenderer, surface);
+      if (texture == nullptr) {
+        std::cout << "Unable to create texture from image, " << path << "."
+                  << "SDL Error: " << SDL_GetError() << std::endl;
+      } else {
+        this->width = surface->w;
+        this->height = surface->h;
+      }
+
+      SDL_FreeSurface(surface);
+    }
+
+    this->texture = texture;
+
+    return texture != nullptr;
+  }
+
+  void free()
+  {
+    if (this->texture != nullptr) {
+      SDL_DestroyTexture(this->texture);
+
+      this->texture = nullptr;
+      this->width = 0;
+      this->height = 0;
+    }
+  }
+
+  void render(int x, int y)
+  {
+    SDL_Rect renderQuad{x, y, this->width, this->height};
+    SDL_RenderCopy(gRenderer, this->texture, nullptr, &renderQuad);
+  }
+
+  int getWidth()
+  {
+    return this->width;
+  }
+
+  int getHeight()
+  {
+    return this->height;
+  }
+
+private:
+  SDL_Texture* texture;
+
+  int width;
+  int height;
+};
+
+LTexture gFooTexture;
+LTexture gBackGroundTexture;
 
 bool initApp();
 bool loadMedia();
@@ -38,19 +118,11 @@ int main(int argc, char* args[])
           }
         }
 
-        SDL_Rect topLeftViewport{0, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
-        SDL_RenderSetViewport(gRenderer, &topLeftViewport);
-        SDL_RenderCopy(gRenderer, gTexture, nullptr, nullptr);
+        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(gRenderer);
 
-        SDL_Rect topRightViewport{SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2,
-                                  SCREEN_HEIGHT / 2};
-        SDL_RenderSetViewport(gRenderer, &topRightViewport);
-        SDL_RenderCopy(gRenderer, gTexture, nullptr, nullptr);
-
-        SDL_Rect bottomViewport{0, SCREEN_HEIGHT / 2, SCREEN_WIDTH,
-                                SCREEN_HEIGHT / 2};
-        SDL_RenderSetViewport(gRenderer, &bottomViewport);
-        SDL_RenderCopy(gRenderer, gTexture, nullptr, nullptr);
+        gBackGroundTexture.render(0, 0);
+        gFooTexture.render(240, 190);
 
         SDL_RenderPresent(gRenderer);
       }
@@ -105,9 +177,13 @@ bool loadMedia()
 {
   bool loadingSuccessState = true;
 
-  gTexture = loadTexture("data/textures/sample_texture.png");
-  if (gTexture == nullptr) {
-    std::cout << "Failed to load texture image." << std::endl;
+  if (!gFooTexture.loadFromFile("data/textures/foo.png")) {
+    std::cout << "Failed to load Foo texture image." << std::endl;
+    loadingSuccessState = false;
+  }
+
+  if (!gBackGroundTexture.loadFromFile("data/textures/background.png")) {
+    std::cout << "Failed to load background texture image." << std::endl;
     loadingSuccessState = false;
   }
 
@@ -116,8 +192,8 @@ bool loadMedia()
 
 void closeApp()
 {
-  SDL_DestroyTexture(gTexture);
-  gTexture = nullptr;
+  gFooTexture.free();
+  gBackGroundTexture.free();
 
   SDL_DestroyRenderer(gRenderer);
   SDL_DestroyWindow(gWindow);
